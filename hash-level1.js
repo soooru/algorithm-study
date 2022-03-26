760637736ba5979bfc8bcae1bca796c69aa9c67e
sooru <threequaters9@gmail.com>
2022-03-27 00:20:49 +0900

    first commit

--- hash-level1.js
+++ hash-level1.js
@@ -1,6 +1,6 @@
 // 프로그래머스 > 코딩 테스트 연습 > 해시 > 완주하지 못한 선수
 // https://programmers.co.kr/learn/courses/30/lessons/42576
-//
+
 function solution(participant, completion) {
   var answer = '';
   participant = participant.sort();
