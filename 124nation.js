072a89a8208c8a3018435bd48c9136effb968458
eunsol <eunsol@bbangyanetworks.com>
2022-03-26 20:33:59 +0900

    main

---
+++ 124nation.js
@@ -0,0 +1,17 @@
+// 프로그래머스 > 코딩 테스트 연습 > 연습문제 > 124 나라의 숫자
+// https://programmers.co.kr/learn/courses/30/lessons/12899
+// 22.03.24
+function solution(n) {
+  if (n <= 500000000) {
+    let answer = '';
+    for (let i = n; i > 0; i = parseInt(i / 3)) {
+      let result = i % 3;
+      if (result === 0) {
+        result = 4;
+        i--;
+      }
+      answer = result + answer;
+    }
+    return answer;
+  }
+}
---
+++ hash-level1.js
@@ -0,0 +1,12 @@
+// 프로그래머스 > 코딩 테스트 연습 > 해시 > 완주하지 못한 선수
+// https://programmers.co.kr/learn/courses/30/lessons/42576
+function solution(participant, completion) {
+  var answer = '';
+  participant = participant.sort();
+  completion = completion.sort();
+  const answerIndex = participant.findIndex((v, index) => {
+    return !completion[index] || v !== completion[index];
+  });
+  answer = participant[answerIndex];
+  return answer;
+}
