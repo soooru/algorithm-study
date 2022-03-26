8122d2af361fb93c68a4bb79d7ebaf423cb01a7e
sooru <threequaters9@gmail.com>
2022-03-27 00:16:22 +0900

    first.. commit

--- 124nation.js
+++ 124nation.js
@@ -1,6 +1,7 @@
 // 프로그래머스 > 코딩 테스트 연습 > 연습문제 > 124 나라의 숫자
 // https://programmers.co.kr/learn/courses/30/lessons/12899
 // 22.03.24
+//
 function solution(n) {
   if (n <= 500000000) {
     let answer = '';
